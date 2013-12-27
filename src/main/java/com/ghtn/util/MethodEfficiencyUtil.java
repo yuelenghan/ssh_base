894091b0a79aad133a0fdd9f1ff983140d5ee22e
yuelenghan <lh_1986@foxmail.com>
2013-12-27 17:55:36 +0800

    增加执行方法超过10秒的警告

--- src/main/java/com/ghtn/util/MethodEfficiencyUtil.java
+++ src/main/java/com/ghtn/util/MethodEfficiencyUtil.java
@@ -1,16 +1,24 @@
 package com.ghtn.util;
 
-import org.apache.log4j.Logger;
+import org.apache.commons.logging.Log;
+import org.apache.commons.logging.LogFactory;
 import org.aspectj.lang.ProceedingJoinPoint;
 
 /**
  * 用于观察方法的执行效率
  *
- * @author yuelenghan
+ * @author lh
  */
 public class MethodEfficiencyUtil {
-    private static Logger logger = Logger.getLogger(LogUtil.class);
+    private static Log log = LogFactory.getLog(LogUtil.class);
 
+    /**
+     * 包裹方法
+     *
+     * @param joinPoint 织入点
+     * @return 方法执行的返回值
+     * @throws Throwable 抛出所有异常
+     */
     public Object around(ProceedingJoinPoint joinPoint) throws Throwable {
         long start = System.currentTimeMillis();
 
@@ -18,9 +26,15 @@ public class MethodEfficiencyUtil {
 
         long end = System.currentTimeMillis();
 
-        logger.debug("=======执行[" + joinPoint.getTarget().getClass() + "]类的["
+        log.debug("执行[" + joinPoint.getTarget().getClass() + "]类的["
                 + joinPoint.getSignature().getName() + "]方法用时"
-                + (end - start) + "ms=======");
+                + (end - start) + "ms");
+
+        // 如果一个方法执行时间超过10秒
+        if (end - start > 10000) {
+            log.warn("执行[" + joinPoint.getTarget().getClass() + "]类的["
+                    + joinPoint.getSignature().getName() + "]方法耗时超过10秒!!!!!!!!!!!!!!!!");
+        }
 
         return returnValue;
 
